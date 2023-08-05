e1b67ce002a24ec3390ad6e9f24b52252815836f
Cameron Dugan <cameron.dugan@protonmail.com>
2023-08-05 15:06:17 -0400

    Added autosave

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,20 +1,30 @@
 return {
-  -- Obsidian Config
-  "epwalsh/obsidian.nvim",
-  lazy = true,
-  event = { "BufReadPre " .. vim.fn.expand "~" .. "/Notes/**.md" },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  keys = {
-    {"gf", "<cmd>ObsidianFollowLink<cr>", desc = "Go to File"},
-  },
-  opts = {
-    dir = "~/Notes",  -- no need to call 'vim.fn.expand' here
-    daily_notes = {
-      folder = "Week Planner/Days"
+  -- Obsidian
+  {
+    "epwalsh/obsidian.nvim",
+    lazy = true,
+    event = { "BufReadPre " .. vim.fn.expand "~" .. "/Notes/**.md" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
     },
-    mappings = {
+    keys = {
+      {"gf", "<cmd>ObsidianFollowLink<cr>", desc = "Go to File"},
     },
+    opts = {
+      dir = "~/Notes",  -- no need to call 'vim.fn.expand' here
+      daily_notes = {
+        folder = "Week Planner/Days"
+      },
+      mappings = {
+      },
+    },
+  },
+  -- auto-save
+  {
+    "Pocco81/auto-save.nvim",
+    lazy = false,
+    config = function()
+      require("auto-save").setup {}
+    end,
   },
 }
