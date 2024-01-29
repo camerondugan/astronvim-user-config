055fea9876b0b0ff441a2df0f0bba517975907a8
Cameron Dugan <cameron.dugan@protonmail.com>
2024-01-29 14:26:11 -0500

    cleaned up config

--- plugins/user.lua
+++ plugins/user.lua
@@ -25,4 +25,34 @@ return {
     lazy = false,
     dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
+  -- neorg: from community with my dir
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "VeryLazy",
+    opts = {
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.keybinds"] = {}, -- Adds default keybindings
+        ["core.summary"] = {}, -- Adds generate-workspace-summary
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        }, -- Enables support for completion plugins
+        ["core.journal"] = {}, -- Enables support for the journal module
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/Notes",
+            },
+            default_workspace = "notes",
+          },
+        },
+      },
+    },
+  },
+
 }
